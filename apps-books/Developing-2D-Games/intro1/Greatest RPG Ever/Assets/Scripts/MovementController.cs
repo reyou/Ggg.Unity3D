11f58e131dc76344a936588552953d4d726d5793
Teoman Shipahi <aytekoz@yahoo.com>
2021-02-09 23:14:44 -0600

    animations added

--- apps-books/Developing-2D-Games/intro1/Greatest RPG Ever/Assets/Scripts/MovementController.cs
+++ apps-books/Developing-2D-Games/intro1/Greatest RPG Ever/Assets/Scripts/MovementController.cs
@@ -1,18 +1,75 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    public float movementSpeed = 3.0f;
+    Vector2 movement;
+    private Rigidbody2D rb2D;
+    private Animator animator;
+
+    private string animationState = "AnimationState";
+
+    enum CharStates
+    {
+        walkEast = 1,
+        walkSouth = 2,
+        walkWest = 3,
+        walkNorth = 4,
+        idleSouth = 5
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        UpdateState();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MoveCharacter();
     }
+
+    private void MoveCharacter()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        /*This will “normalize” our vector and keep the player moving at the same rate
+         of speed whether they’re moving diagonally, vertically, or horizontally.*/
+        movement.Normalize();
+
+        rb2D.velocity = movement * movementSpeed;
+    }
+
+    private void UpdateState()
+    {
+        if (movement.x > 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkEast);
+        }
+        else if (movement.x < 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkWest);
+        }
+        else if (movement.y > 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkNorth);
+        }
+        else if (movement.y < 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkSouth);
+        }
+        else
+        {
+            animator.SetInteger(animationState, (int)CharStates.idleSouth);
+        }
+    }
+
 }
