27c09862268b25af84e48df427d035cd79a20148
Teoman Shipahi <aytekoz@yahoo.com>
2021-02-21 23:29:23 -0600

    Colliders  and Tilemaps

---
+++ apps-books/Developing-2D-Games/intro1/Greatest RPG Ever/Assets/Scripts/RoundCameraPos.cs
@@ -0,0 +1,23 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class RoundCameraPos : CinemachineExtension
+{
+    public float PixelsPerUnit = 32;
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            Vector3 pos = state.FinalPosition;
+            Vector3 pos2 = new Vector3(Round(pos.x), Round(pos.y), pos.z);
+
+            state.PositionCorrection += pos2 - pos;
+            // Debug.Log($"pos: {pos}, pos2: {pos2}, {nameof(state.PositionCorrection)}: {state.PositionCorrection}");
+        }
+    }
+
+    private float Round(float x)
+    {
+        return Mathf.Round(x * PixelsPerUnit) / PixelsPerUnit;
+    }
+}
