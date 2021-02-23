b74959618dd08b548f320e464b0b4d17e4c94741
Teoman Shipahi <aytekoz@yahoo.com>
2021-02-22 22:22:33 -0600

    character classes

---
+++ apps-books/Developing-2D-Games/intro1/Greatest RPG Ever/Assets/Scripts/MonoBehaviours/Character.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This Character class will contain functionality and properties
+/// common to all character types in our game.
+/// </summary>
+public class Character : MonoBehaviour
+{
+
+    public int hitPoints;
+    public int maxHitPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
