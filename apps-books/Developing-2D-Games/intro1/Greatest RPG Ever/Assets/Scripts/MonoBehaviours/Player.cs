93749885755a02df5a2a4d05a6dc408b233f8a47
Teoman Shipahi <aytekoz@yahoo.com>
2021-02-22 23:48:38 -0600

    OnTriggerEnter2D

--- apps-books/Developing-2D-Games/intro1/Greatest RPG Ever/Assets/Scripts/MonoBehaviours/Player.cs
+++ apps-books/Developing-2D-Games/intro1/Greatest RPG Ever/Assets/Scripts/MonoBehaviours/Player.cs
@@ -1,4 +1,12 @@
-﻿public class Player : Character
-{
+﻿using UnityEngine;
 
+public class Player : Character
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
 }
